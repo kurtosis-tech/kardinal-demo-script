3bb6221e753141050ff2d1e31a093a91af39bb19
Tedi Mitiku <tedi.m52@gmail.com>
2024-06-20 23:46:24 -0400

    add err handling

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 git clone https://github.com/kurtosis-tech/kardinal-demo-script.git
-cd kardinal-demo-script
+if [ $? -ne 0 ]; then
+    echo "Failed to clone repository. Exiting."
+    exit 1
+fi
+
+cd $PWD/kardinal-demo-script
+if [ $? -ne 0 ]; then
+    echo "Failed to change directory to kardinal-demo-script. Exiting."
+    exit 1
+fi
+
 chmod u+x kardinal-cli
 export PATH=$PWD:$PATH
